91890242faf8e8b5825cd89933801d87ad5c9524
Adam Vermes <vermesadi@gmail.com>
2017-07-04 18:21:53 +0200

    Judy's 3rd question

--- printing.py
+++ printing.py
@@ -1,2 +1,9 @@
 
 # Printing functions
+
+from reports import count_games
+from reports import decide
+from reports import get_latest
+print(count_games('game_stat.txt'))
+print(decide('game_stat.txt', 1998))
+print(get_latest('game_stat.txt'))
--- reports.py
+++ reports.py
@@ -3,3 +3,54 @@
 # How many games are in the file?
 # Expected name of the function: count_games(file_name)
 # Expected output of the function: a number
+import csv
+from tabulate import tabulate
+import re
+
+
+def count_games(file_name):
+    with open(file_name, 'r') as file:
+        game_list = []
+        for line in file:
+            game_list.append(line)
+        num_of_games = (len(game_list))
+        return num_of_games
+
+
+# Is there a game from a given year?
+# Expected name of the function: decide(file_name, year)
+# Expected output of the function: boolean value
+
+
+def decide(file_name, year):
+    with open(file_name, 'r') as file:
+        game_list = []
+        for row in file:
+            game_list.append(row)
+            game_list_string = ''.join(str(element) for element in game_list)
+            game_list_string_split = game_list_string.split()
+        if str(year) in game_list_string:
+            return True
+        if str(year) not in game_list_string:
+            return False
+
+# Which was the latest game?
+# Expected name of the function: get_latest(file_name)
+# Expected output of the function: the title of the latest game as a string
+# Other expectation: if there is more than one, then return the first from the file
+
+
+def get_latest(file_name):
+    game_list = []
+    year_list = []
+    latest_year = []
+    with open(file_name) as file:
+        for strings in file:
+            game_list.append(strings.split('\t'))
+        for title, sold, year, genre, publisher in game_list:
+            year_list.append(int(year))
+            year_list.sort(reverse=True)
+            latest_year = str(year_list[0])
+        for title, sold, year, genre, publisher in game_list:
+            if latest_year == year:
+                return title
