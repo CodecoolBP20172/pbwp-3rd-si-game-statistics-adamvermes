3850de00d6ddd919f1c75c9886d7ed60c5ddad3d
Adam Vermes <vermesadi@gmail.com>
2017-07-05 16:13:20 +0200

    Judy's 5th question

--- reports.py
+++ reports.py
@@ -31,6 +31,7 @@ def decide(file_name, year):
         if str(year) not in game_list_string:
             return False
 
+
 # Which was the latest game?
 # Expected name of the function: get_latest(file_name)
 # Expected output of the function: the title of the latest game as a string
@@ -44,7 +45,7 @@ def get_latest(file_name):
     with open(file_name) as file:
         for strings in file:
             game_list.append(strings.split('\t'))
-        for title, sold, year, genre, publisher in game_list:
+        for title, sold, year, game_genre, publisher in game_list:
             year_list.append(int(year))
             year_list.sort(reverse=True)
             latest_year = str(year_list[0])
@@ -57,6 +58,7 @@ def get_latest(file_name):
 # Expected name of the function: count_by_genre(file_name, genre)
 # Expected output of the function: a number
 
+
 def count_by_genre(file_name, genre):
     game_list = []
     genre_list = []
@@ -66,3 +68,25 @@ def count_by_genre(file_name, genre):
         for title, sold, year, game_genre, publisher in game_list:
             genre_list.append(game_genre)
         return genre_list.count(genre)
+
+
+# What is the line number of the given game (by title)?
+# Expected name of the function: get_line_number_by_title(file_name, title)
+# Expected output of the function: a number (if there is no game found, then raises ValueError exception)
+
+
+def get_line_number_by_title(file_name, title):
+    game_list = []
+    game_title_list = []
+    game_line = []
+    with open(file_name) as file:
+        for strings in file:
+            game_list.append(strings.split('\t'))
+        for game_title, sold, year, game_genre, publisher in game_list:
+            game_title_list.append(game_title)
+            print(game_title_list)
+        if title in game_list:
+            game_line = game_list.index(title) + 1
+            print(game_line)
+        else:
+            print("Not in list")
