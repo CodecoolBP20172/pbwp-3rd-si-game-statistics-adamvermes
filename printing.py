2f641ad220d300b4e6612f81e6d988c039837577
Adam Vermes <vermesadi@gmail.com>
2017-07-05 17:06:10 +0200

    Judy's 5 questions

--- printing.py
+++ printing.py
@@ -11,5 +11,5 @@ from reports import get_line_number_by_title
 print(count_games('game_stat.txt'))
 print(decide('game_stat.txt', 1998))
 print(get_latest('game_stat.txt'))
-print(count_by_genre('game_stat.txt', 'RPG'))
-print(get_line_number_by_title('game_stat.txt', 'Diablo III'))
+print(count_by_genre('game_stat.txt', genre))
+print(get_line_number_by_title('game_stat.txt', title))
