ae5ba4c9d5ca14db18db9e85785d0636b386b1f3
Adam Vermes <vermesadi@gmail.com>
2017-07-07 22:35:56 +0200

    4th question

--- part2/reports.py
+++ part2/reports.py
@@ -67,3 +67,33 @@ def get_selling_avg(file_name):
         games_sold_list_total = sum(games_sold_list_float)
         avg = games_sold_list_total / len(game_list)
         return avg
+
+
+# How many characters long is the longest title?
+# Expected name of the function: count_longest_title(file_name)
+# Expected output of the function: (number)
+
+
+def count_longest_title(file_name):
+    game_list = []
+    games_len_list = []
+    games_lengths = []
+    games_lengths_max = []
+    with open(file_name) as file:
+        for strings in file:
+            game_list.append(strings.strip().split('\t'))
+        for game_title, games_sold, game_year, game_genre, game_publisher in game_list:
+            games_len_list.append(game_title)
+        for titles in games_len_list:
+            games_lengths.append(len(titles))
+            games_lengths_max = max(games_lengths)
+        return games_lengths_max
+
+
+# What is the average of the release dates?
+# Expected name of the function: get_date_avg(file_name)
+# Expected output of the function: average year (number)
+# Other expectation: the return value must be the rounded up average
+
+def get_date_avg(file_name):
+    
