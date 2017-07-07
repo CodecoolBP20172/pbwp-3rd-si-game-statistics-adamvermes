e317b98732ef0fd4c8d2431f0faa335ba945e6e2
Adam Vermes <vermesadi@gmail.com>
2017-07-07 23:26:16 +0200

    6th question

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,8 @@
+import reports
 
-# Export functions
+with open('export.txt', 'w') as export_file:
+    export_file.write(str(reports.get_most_played('game_stat.txt')) + '\n')
+    export_file.write(str(reports.sum_sold('game_stat.txt')) + '\n')
+    export_file.write(str(reports.get_selling_avg('game_stat.txt')) + '\n')
+    export_file.write(str(reports.get_date_avg('game_stat.txt')) + '\n')
+    export_file.write(str(reports.get_game('game_stat.txt', 'Counter-Strike')) + '\n')
