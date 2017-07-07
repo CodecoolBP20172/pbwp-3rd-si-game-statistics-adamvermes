44023da610b07e841516689bdb057fc3ef5741d1
Adam Vermes <vermesadi@gmail.com>
2017-07-07 21:26:03 +0200

    2nd question

--- part2/printing.py
+++ part2/printing.py
@@ -1,3 +1,4 @@
 import reports
 
 print(reports.get_most_played('game_stat.txt'))
+print(reports.sum_sold('game_stat.txt'))
--- part2/reports.py
+++ part2/reports.py
@@ -6,6 +6,7 @@
 # Expected output of the function: (string)
 # Other expectation:  if there is more than one, then return the first from the file
 
+
 def get_most_played(file_name):
     game_list = []
     games_sold_list = []
@@ -24,4 +25,27 @@ def get_most_played(file_name):
         for game_title, games_sold, game_year, game_genre, game_publisher in game_list:
             if max_list == games_sold:
                 return game_title
-    
+
+
+# How many copies have been sold total?
+# Expected name of the function: sum_sold(file_name)
+# Expected output of the function: (number)
+
+def sum_sold(file_name):
+    game_list = []
+    games_sold_list = []
+    games_sold_list_float = []
+    games_sold_list_total = []
+    with open(file_name) as file:
+        for strings in file:
+            game_list.append(strings.strip().split('\t'))
+        for game_title, games_sold, game_year, game_genre, game_publisher in game_list:
+            games_sold_list.append(games_sold)
+        for numbers in games_sold_list:
+            games_sold_list_float.append(float(numbers))
+        games_sold_list_total = sum(games_sold_list_float)
+        return games_sold_list_total
+
+# What is the average selling?
+# Expected name of the function: get_selling_avg(file_name)
+# Expected output of the function: (number)
